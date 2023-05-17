4c39c0984c7d8bbd1aa20ec437ee7debbe468ae7
ctecchiog <127995834+ctecchiog@users.noreply.github.com>
2023-05-17 14:29:14 -0300

    Update and rename Inim_TipoA.cpp to Inim_Facil.cpp

---
+++ Inim_Facil.cpp
@@ -0,0 +1,41 @@
+#include "Inim_Facil.h"
+
+Inim_Facil::Inim_Facil()
+{
+
+}
+
+Inim_Facil::~Inim_Facil()
+{
+
+}
+
+void Inim_Facil::lancaProjetil(Projetil *pP)
+{
+
+}
+
+bool Inim_Facil::acertou(Projetil *pP, Jogador *pJ, bool *hit)
+{
+
+}
+
+int Inim_Facil::getID()
+{
+    return this->id;
+}
+
+int Inim_Facil::getX()
+{
+    return this->x;
+}
+
+int Inim_Facil::getY()
+{
+    return this->y;
+}
+
+int Inim_Facil::getVidas()
+{
+    return this->num_vidas;
+}
