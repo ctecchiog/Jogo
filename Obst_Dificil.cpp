5bc51fe515c9e81cbc99c833817a5c0c045b4f00
ctecchiog <127995834+ctecchiog@users.noreply.github.com>
2023-05-17 15:00:53 -0300

    Update and rename Obst_TipoB.cpp to Obst_Dificil.cpp

---
+++ Obst_Dificil.cpp
@@ -0,0 +1,28 @@
+#include "Obst_Dificil.h"
+
+Obst_Dificil::Obst_Dificil()
+{
+}
+
+Obst_Dificil::~Obst_Dificil()
+{
+}
+
+void Obst_Dificil::executar()
+{
+}
+
+int Obst_Dificil::getID()
+{
+    return this->id;
+}
+
+int Obst_Dificil::getX()
+{
+    return this->x;
+}
+
+int Obst_Dificil::getY()
+{
+    return this->y;
+}
