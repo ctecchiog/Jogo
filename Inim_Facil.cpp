6b6bce1afa274d584b6c058c9034da94bb51b88f
ctecchiog <127995834+ctecchiog@users.noreply.github.com>
2023-05-17 14:40:28 -0300

    Update and rename Inim_TipoB.cpp to Inim_Facil.cpp

---
+++ Inim_Facil.cpp
@@ -0,0 +1,31 @@
+#include "Inim_Facil.h"
+
+Inim_Facil::Inim_Facil()
+{
+
+}
+
+Inim_Facil::~Inim_Facil()
+{
+
+}
+
+int Inim_Facil::getID()
+{
+    return this->id;
+}
+
+int Inim_Facil::getX()
+{
+    return this->x;
+}
+
+int Inim_Facil::getY()
+{
+    return this->y;
+}
+
+int Inim_Facil::getVidas()
+{
+    return this->num_vidas;
+}
