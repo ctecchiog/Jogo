748026fd7df8fe64b222386acb20bddee6c443e7
ctecchiog <127995834+ctecchiog@users.noreply.github.com>
2023-05-17 14:37:12 -0300

    Update and rename Inim_Facil.h to Inim_Dificil.h

--- Inim_Dificil.h
+++ Inim_Dificil.h
@@ -4,14 +4,14 @@
 #include "Projetil.h"
 #include "Jogador.h"
 
-class Inim_Facil: public Inimigo{
+class Inim_Dificil: public Inimigo{
 private:
     bool hit;
     Projetil *proj;
     Jogador *player;
 public:
-    Inim_Facil();
-    ~Inim_Facil();
+    Inim_Dificil();
+    ~Inim_Dificil();
     void executar();
     void lancaProjetil(Projetil *pP);
     bool acertou(Projetil *pP, Jogador *pJ, bool *hit);
