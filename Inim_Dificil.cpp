ab0193069025650c68e60a31b810cf1fafd87d11
ctecchiog <127995834+ctecchiog@users.noreply.github.com>
2023-05-17 14:38:30 -0300

    Update and rename Inim_Facil.cpp to Inim_Dificil.cpp

---
+++ Inim_Dificil.cpp
@@ -0,0 +1,46 @@
+#include "Inim_Dificil.h"
+
+Inim_Dificil::Inim_Dificil()
+{
+
+}
+
+Inim_Dificil::~Inim_Dificil()
+{
+
+}
+
+void Inin_Dificil::executar()
+{
+   
+}
+
+void Inim_Dificil::lancaProjetil(Projetil *pP)
+{
+
+}
+
+bool Inim_Dificil::acertou(Projetil *pP, Jogador *pJ, bool *hit)
+{
+
+}
+
+int Inim_Dificil::getID()
+{
+    return this->id;
+}
+
+int Inim_Dificil::getX()
+{
+    return this->x;
+}
+
+int Inim_Dificil::getY()
+{
+    return this->y;
+}
+
+int Inim_Dificil::getVidas()
+{
+    return this->num_vidas;
+}
